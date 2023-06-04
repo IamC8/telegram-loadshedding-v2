967a3671a910307d3cf9c5c0bd42f873fbd1e16b
IamC8 <github.10.n0spamt0day@spamgourmet.com>
2023-06-04 22:55:01 +0200

    Building up Telegram database, changed html to check for active links.

--- database.py
+++ database.py
@@ -15,7 +15,7 @@ def load_db(search):
     search = "%" + search + "%"
     with engine.connect() as conn:
         # result = conn.execute(text("select * from telethon where bagz = '%s'" % search))
-        result = conn.execute(text("select distinct t1.bagz, t2.provider, t2.province, t2.muni, t2.name, t1.id from telethon t1 join subburbs t2 on t1.aid = t2.aid where t1.bagz = t2.bagz AND t2.name like '%s' ORDER BY t2.province ASC" % search))
+        result = conn.execute(text("select distinct t1.bagz, t2.provider, t2.province, t2.muni, t2.name, t1.link from telethon t1 join subburbs t2 on t1.aid = t2.aid where t1.bagz = t2.bagz AND t2.name like '%s' ORDER BY t2.province, t2.name ASC" % search))
         columns = [column[0] for column in result.cursor.description]
         ret = []
         for row in result.fetchall():
