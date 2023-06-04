9a1f542ea08477a67741d170757edba3f3b9cf3b
IamC8 <github.10.n0spamt0day@spamgourmet.com>
2023-06-04 13:14:23 +0200

    Search working fine. Changed some text.

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, jsonify, request
 from database import load_db
 from forms import *
 
-
 app = Flask(__name__)
 
 
@@ -10,10 +9,12 @@ app = Flask(__name__)
 def hello_world():
     return render_template("home.html")
 
+
 @app.route("/api")
 def list_json():
     tel = load_db()
     return jsonify(tel)
+
 
 @app.route('/search', methods=['GET', 'POST'])
 def search():
@@ -21,10 +22,15 @@ def search():
         req = request.form
         input = req["area"]
         res = load_db(input)
-        return render_template('home.html', jobs=res)
+        # for r in res:
+        #     print(r)
+        return render_template('home.html', results=res)
     else:
         return render_template('home.html')
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
 
 #  https://www.youtube.com/watch?v=yBDHkveJUf4
+# <tr onclick="window.open('https://t.co/' + {{ res['id'] }})">
